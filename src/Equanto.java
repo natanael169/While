c0d0b2f1de16a638df7ab5cc384a8af928915c9b
natanael <natanjosee169@gmail.com>
2021-06-23 01:19:54 -0300

    Initial commit

---
+++ src/Equanto.java
@@ -0,0 +1,30 @@
+public class Equanto {
+    public static void main(String[] args) {
+         //int n1 = 10;
+
+
+        //é um tipo de laço como o do-while
+        int n2;
+        for (n2 =0; n2<=20; n2 += 2 ){ // ++ é variavel mais um, += é variavel mais qualquer numero
+             System.out.println("Valor:"+ n2);
+         }
+
+         //estrutura de repetição,que vai repetir enquanto a expressão booleana for verdadeira
+
+        /* while (n1< 15){
+             System.out.println("não pode entrar");
+             n1++;
+         }*/
+
+        //as intruções são avaliadas logo após a primeira execução, mesmo que o laço seja falso
+        //ele executara as instruções pelo menos uma vez
+      /*  do {
+            System.out.println("nao pode entrar");
+            n1++;
+        } while (n1<15); */
+
+
+
+
+    }
+}
